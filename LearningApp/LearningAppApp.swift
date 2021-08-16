184c2d86765387981c27f272bb801c18d4af4628
Lilian Grasset <lilian.grasset@icloud.com>
2021-08-16 14:37:56 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Lilian Grasset on 16/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
